ae43ec6ead5938f5046dce983efc833e8827bb91
Yahihs <92796916+Yahihs@users.noreply.github.com>
2021-12-24 01:17:41 +0530

    Update yt.py

--- yt.py
+++ yt.py
@@ -4,13 +4,13 @@ import random
 import threading
 driver = uc.Chrome()
 
-vlist = ['https://youtu.be/lUxzjQvNBmg','https://youtu.be/PHBLddvszao','https://youtu.be/uWcyQKQBnXk']
+vlist = ['https://youtu.be/6G-pTxdjao4','https://youtu.be/eJeBFr4FgDI','https://youtu.be/iL4kBcNpm2s','https://youtu.be/1V-tQzN6Azw']
 tabs = 4
 
 def view(driver):
 	for i in range(2000):
 		driver.get(vlist[random.randint(0,2)])
-		time.sleep(random.randint(20,40))
+		time.sleep(random.randint(2700,3700))
 
 for i in range(tabs):
 	browser=threading.Thread(target=view,args=(uc.Chrome(),))
