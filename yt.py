e5e6dd69e00dfee0be6d2f293fe16314ab59ad33
Yahihs <92796916+Yahihs@users.noreply.github.com>
2021-12-24 01:24:25 +0530

    Update yt.py

--- yt.py
+++ yt.py
@@ -1,8 +1,9 @@
-import undetected_chromedriver as uc
+import undetected_chromedriver.v2 as uc
+
 import time
 import random
 import threading
-driver = uc.Chrome()
+driver = uc.Chrome(version_main=96)
 
 vlist = ['https://youtu.be/6G-pTxdjao4','https://youtu.be/eJeBFr4FgDI','https://youtu.be/iL4kBcNpm2s','https://youtu.be/1V-tQzN6Azw']
 tabs = 4
@@ -13,7 +14,7 @@ def view(driver):
 		time.sleep(random.randint(2700,3700))
 
 for i in range(tabs):
-	browser=threading.Thread(target=view,args=(uc.Chrome(),))
+	browser=threading.Thread(target=view,args=(uc.Chrome(version_main=96),))
 	browser.start()
 
 driver.quit()
